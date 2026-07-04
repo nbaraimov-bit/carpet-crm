15ace11e254e9ec82270c6c7e29e9821f9ecbc3d
Nurmuhammad <nbaraimov@gmail.com>
2026-07-04 12:00:54 +0500

    teams yaratish 3

---
+++ src/components/TeamsPanel/CreateTeamModal.jsx
@@ -0,0 +1,145 @@
+export default function CreateTeamModal({
+  showCreateTeam,
+  setShowCreateModal,
+  teamName,
+  setTeamName,
+  selectedType,
+  handleCreateTeam,
+  isNameValid,
+  teamTypes,
+  loading,
+  setLoading,
+}) {
+
+  return(
+
+    <div
+      style={{
+        position: "fixed",
+        inset: 0,
+        background: "rgba(0,0,0,0.6)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 999,
+      }}
+    >
+      <div
+        style={{
+          width: "90%",
+          maxWidth: 380,  
+          background: "#111827",
+          borderRadius: 20,
+          padding: 20,
+        }}
+      >
+        <h2>👥 Jamoa yaratish</h2>
+
+        <p>Jamoa nomi</p>
+
+        <input
+          className={
+            nameLength === 0
+            ? "team-name-input"
+            : isNameValid
+            ? "team-name-input valid"
+            : "team-name-input invalid"
+          }
+          placeholder="Masalan: fatality"
+          value={teamName}
+          onChange={(e) => setTeamName(e.target.value)}
+        />
+
+        {nameLength > 0 && nameLength < 5 && (
+          <p className="input-error">
+            Kamida 5 ta belgi kiriting
+          </p>
+        )}
+
+        {nameLength > 20 && (
+          <p className="input-error">
+            Eng ko'pi 20 ta belgi bo'lishi mumkin
+          </p> 
+        )}
+
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: 10,
+            marginTop: 20,
+          }}
+        >
+          {teamTypes.map((type) => { 
+
+            const disabled = joinedTypes.includes(type.id)  
+
+            return(
+
+              <div
+                key={type.id}
+                onClick={() => {
+                  if(disabled) return
+                  setSelectedType(type.id)
+                }}
+
+                className={
+                  disabled ? "team-type-card disabled"
+                  :selectedType === type.id
+                  ? "team-type-card active"
+                  : "team-type-card"
+                }
+              >
+                <div className="team-content">
+                  <div className="team-type-icon">
+                    {type.icon}
+                  </div>    
+
+                  <div className="team-type-title">
+                    {type.title}
+                  </div>
+                </div> 
+
+                {disabled && (
+                  <div className="team-stamp">
+                    A'ZO
+                  </div>
+                )}
+
+              </div>
+
+            )
+
+          })}
+        </div>
+
+        <div className="create-team-buttons">
+
+          <button
+            className="cancel-button"
+            onClick={handleCloseCreateModal}
+          >
+            Bekor
+          </button>
+
+          <button
+            className="create-button"
+            disabled={
+              loading ||
+              teamName.trim().length < 5 ||
+              teamName.trim().length > 20 ||
+              !selectedType
+            }
+            onClick={handleCreateTeam}
+          >
+            {loading ? "Yaratilmoqda..." : "Yaratish"}
+          </button>
+
+        </div>
+
+      </div>
+    </div>
+
+  )
+
+}
--- src/components/TeamsPanel/TeamsPanel.jsx
+++ src/components/TeamsPanel/TeamsPanel.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 import "./teams.css"
 import PendingTeamCard from "./PendingTeamCard";
 import teamTypes, { teamTypeMap } from "./TeamTypes";
+import CreateTeamModal from "./CreateTeamModal";
 
 import {
   doc,
@@ -45,28 +46,6 @@ export default function TeamsPanel({
 
   const isAdmin = allowedRoles.includes("admin") || allowedRoles.includes("ega")
   const isWorker = allowedRoles.includes("worker")
-
-  const teamTypes = [
-    {
-      id: "washer",
-      title: "Gilam yuvish",
-      icon: "🧼",
-    },
-    {
-      id: "driver",
-      title: "Yetkazib berish",
-      icon: "🚚",
-    },
-    {
-      id: "packing",
-      title: "Tayyorlovchi",
-      icon: "📦",
-    },
-  ]
-
-  const teamTypeMap = Object.fromEntries(
-    teamTypes.map((type) => [type.id, type])
-  )
 
   useEffect(() => {
 
@@ -264,132 +243,20 @@ export default function TeamsPanel({
         </div>
 
         {showCreateTeam && (
-          <div
-            style={{
-              position: "fixed",
-              inset: 0,
-              background: "rgba(0,0,0,0.6)",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              zIndex: 999,
-            }}
-          >
-            <div
-              style={{
-                width: "90%",
-                maxWidth: 380,  
-                background: "#111827",
-                borderRadius: 20,
-                padding: 20,
-              }}
-            >
-              <h2>👥 Jamoa yaratish</h2>
+          <CreateTeamModal
 
-              <p>Jamoa nomi</p>
-
-              <input
-                className={
-                  nameLength === 0
-                  ? "team-name-input"
-                  : isNameValid
-                  ? "team-name-input valid"
-                  : "team-name-input invalid"
-                }
-                placeholder="Masalan: fatality"
-                value={teamName}
-                onChange={(e) => setTeamName(e.target.value)}
-              />
-
-              {nameLength > 0 && nameLength < 5 && (
-                <p className="input-error">
-                  Kamida 5 ta belgi kiriting
-                </p>
-              )}
-
-              {nameLength > 20 && (
-                <p className="input-error">
-                  Eng ko'pi 20 ta belgi bo'lishi mumkin
-                </p> 
-              )}
-
-              <div
-                style={{
-                  display: "grid",
-                  gridTemplateColumns: "1fr 1fr",
-                  gap: 10,
-                  marginTop: 20,
-                }}
-              >
-                {teamTypes.map((type) => { 
-
-                  const disabled = joinedTypes.includes(type.id)  
-
-                  return(
-
-                    <div
-                      key={type.id}
-                      onClick={() => {
-                        if(disabled) return
-                        setSelectedType(type.id)
-                      }}
-
-                      className={
-                        disabled ? "team-type-card disabled"
-                        :selectedType === type.id
-                        ? "team-type-card active"
-                        : "team-type-card"
-                      }
-                    >
-                      <div className="team-content">
-                        <div className="team-type-icon">
-                          {type.icon}
-                        </div>    
-
-                        <div className="team-type-title">
-                          {type.title}
-                        </div>
-                      </div> 
- 
-                      {disabled && (
-                        <div className="team-stamp">
-                          A'ZO
-                        </div>
-                      )}
-
-                    </div>
-
-                  )
-
-                })}
-              </div>
-
-              <div className="create-team-buttons">
-
-                <button
-                  className="cancel-button"
-                  onClick={handleCloseCreateModal}
-                >
-                  Bekor
-                </button>
-
-                <button
-                  className="create-button"
-                  disabled={
-                    loading ||
-                    teamName.trim().length < 5 ||
-                    teamName.trim().length > 20 ||
-                    !selectedType
-                  }
-                  onClick={handleCreateTeam}
-                >
-                  {loading ? "Yaratilmoqda..." : "Yaratish"}
-                </button>
-
-              </div>
-
-            </div>
-          </div>
+            showCreateTeam={showCreateTeam}
+            setShowCreateTeam={setShowCreateTeam}
+            teamName={teamName}
+            setTeamName={setTeamName}
+            selectedType={selectedType}
+            handleCreateTeam={handleCreateTeam}
+            isNameValid={isNameValid}
+            teamTypes={teamTypes}
+            loading={loading}
+            setLoading={setLoading}
+  
+          />
         )}
 
         <p>Jamoalarim</p>
