9bc375cc7c9c3997bba76337a4fab57b87e5644b
Nurmuhammad <nbaraimov@gmail.com>
2026-07-04 22:33:33 +0500

    teams paneli 4

--- src/components/TeamsPanel/ActiveTeams/ActiveTeams.jsx
+++ src/components/TeamsPanel/ActiveTeams/ActiveTeams.jsx
@@ -1,6 +1,7 @@
 import TeamAdminPanel from "./TeamAdminPanel";
 import TeamLeaderPanel from "./TeamLeaderPanel";
 import TeamMemberPanel from "./TeamMemberPanel";
+import { teamTypeMap } from "../teamTypes";
 
 export default function ActiveTeams({
   team,
@@ -11,18 +12,23 @@ export default function ActiveTeams({
   const teamMember = team.members?.[currentWorker.phone];
   const isLeader = teamMember?.rank === "leader";
   const isMember = teamMember?.rank === "member";
+  const teamType = teamTypeMap[team.type];
 
   return (
 
     <div className="active-team-card">
 
-        <div className="active-team-accent"></div>
+      <div className="active-team-accent"></div>
 
-            <div className="active-team-header">
+      <div className="active-team-header">
+                
+        <div className="active-team-type">
+          {teamType?.icon} {teamType?.title}
+        </div>
+        
+        {team.teamName}
 
-                🟢 {team.teamName}
-
-            </div>
+      </div>
 
       {isAdmin && <TeamAdminPanel />}
 
