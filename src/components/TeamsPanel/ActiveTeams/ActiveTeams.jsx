938f56cc22d6c84cdb9f03e47f512b4f3a41a890
Nurmuhammad <nbaraimov@gmail.com>
2026-07-04 22:53:35 +0500

    teams paneli 4

--- src/components/TeamsPanel/ActiveTeams/ActiveTeams.jsx
+++ src/components/TeamsPanel/ActiveTeams/ActiveTeams.jsx
@@ -15,43 +15,36 @@ export default function ActiveTeams({
   const teamType = teamTypeMap[team.type];
 
   return (
-    
-    <div>
 
       <div className="active-team-card">
 
-    <div className="active-team-accent"></div>
+        <div className="active-team-accent"></div>
 
-    <div className="active-team-header">
-        <h3>{team.teamName}</h3>
-    </div>
+        <div className="active-team-header">
+          <h3>{team.teamName}</h3>
+        </div>
 
-    <div className="active-team-type">
-        {teamType?.icon} {teamType?.title}
-    </div>
+        <div className="active-team-type">
+          {teamType?.icon} {teamType?.title}
+        </div>
 
-    <div className="active-team-info">
-        👥 {Object.keys(team.members || {}).length} ta a'zo
-    </div>
+        <div className="active-team-info">
+          👥 {Object.keys(team.members || {}).length} ta a'zo
+        </div>
 
-    <div className="active-team-info">
-        👑 {team.leaderName}
-    </div>
+        <div className="active-team-info">
+          👑 {team.leaderName}
+        </div>
 
-    {isAdmin && <TeamAdminPanel />}
+        {isAdmin && <TeamAdminPanel />}
 
-    {isLeader && <TeamLeaderPanel />}
+        {isLeader && <TeamLeaderPanel />}
 
-    {isMember && <TeamMemberPanel />}
+        {isMember && <TeamMemberPanel />}
 
-</div>
+      </div>
 
-      {isAdmin && <TeamAdminPanel />}
-
-      {isLeader && <TeamLeaderPanel />}
-
-      {isMember && <TeamMemberPanel />}
-    </div>
+     
 
   )
   
