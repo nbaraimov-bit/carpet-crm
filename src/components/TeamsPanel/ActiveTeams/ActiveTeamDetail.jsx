1f1f14b4fb821da1f9185c997ba6bba76ccc736f
Nurmuhammad <nbaraimov@gmail.com>
2026-07-05 18:10:44 +0500

    teams paneli 5

--- src/components/TeamsPanel/ActiveTeams/ActiveTeamDetail.jsx
+++ src/components/TeamsPanel/ActiveTeams/ActiveTeamDetail.jsx
@@ -1,3 +1,5 @@
+import "./ActiveTeams.css"
+
 export default function ActiveTeamDetail({
 
     team,
@@ -7,20 +9,67 @@ export default function ActiveTeamDetail({
 
 }) {
 
-    return (
+  const workingCount = Object.values(team.members || {})
+  .filter(member => member.working)
+  .length;
 
-        <div>
+  return (
 
-            <button onClick={closeTeam}>
-                ⬅️
-            </button>
+      <div>
 
-            <h2>{team.teamName}</h2>
+        <button onClick={closeTeam}>
+          ⬅️
+        </button>
 
-            Team Detail
+        <div className="team-detail-summary">
+
+          <div className="team-detail-header">
+
+            <div>
+              <h2>{team.teamName}</h2>
+              <p>{teamType?.icon} {teamType?.title}</p>
+            </div>
+
+            <div className="team-online">
+              🟢 {workingCount} kishi faol
+            </div>
+
+          </div>
+
+          <div className="team-stats-grid">
+
+          <div className="team-stat-box">
+            <h4>🧼 Gilam</h4>
+            <p>0 kv • 0 so'm</p>
+          </div>
+
+          <div className="team-stat-box">
+            <h4>🛏 Adyol</h4>
+            <p>0 ta • 0 so'm</p>
+          </div>
+
+          <div className="team-stat-box">
+            <h4>🧵 Yakandoz</h4>
+            <p>0 ta • 0 so'm</p>
+          </div>
+
+          <div className="team-stat-box">
+            <h4>🪟 Parda</h4>
+            <p>0 m • 0 so'm</p>
+          </div>
 
         </div>
 
-    )
+        <div className="team-total-card">
+
+          💰 Jami: <b>0 so'm</b>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  )
 
 }
