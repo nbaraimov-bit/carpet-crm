522641006f64536ff4ba651022c3ce03f9b29a51
Nurmuhammad <nbaraimov@gmail.com>
2026-07-06 20:07:48 +0500

    teams paneli 4

--- src/components/TeamsPanel/ActiveTeams/ActiveTeamDetail.jsx
+++ src/components/TeamsPanel/ActiveTeams/ActiveTeamDetail.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 import "./ActiveTeams.css"
 import teamTypes, { teamTypeMap } from "../teamTypes";
 import TeamLeaderPanel from "./TeamLeaderPanel";
+import TeamInfoPanel from "./TeamInfoPanel";
 import {
   doc,
   updateDoc,
@@ -36,56 +37,19 @@ export default function ActiveTeamDetail({
 
   return (
 
+    <div>
+
       <div>
 
         <button onClick={closeTeam}>
           ⬅️
         </button>
 
-        <div className="team-detail-summary">
-
-          <div className="team-detail-header">
-
-            <div>
-              <h2>{team.teamName}</h2>
-              <p>{teamType?.icon} {teamType?.title}</p>
-            </div>
-
-            <div className="team-online">
-              🟢 {workingCount} kishi faol
-            </div>
-
-          </div>
-
-          <div className="team-stats-grid">
-
-          <div className="team-stat-box">
-            <h4>🧼 Gilam</h4>
-            <p>0 kv • 0 so'm</p>
-          </div>
-
-          <div className="team-stat-box">
-            <h4>🛏 Adyol</h4>
-            <p>0 ta • 0 so'm</p>
-          </div>
-
-          <div className="team-stat-box">
-            <h4>🧵 Yakandoz</h4>
-            <p>0 ta • 0 so'm</p>
-          </div>
-
-          <div className="team-stat-box">
-            <h4>🪟 Parda</h4>
-            <p>0 m • 0 so'm</p>
-          </div>
-
-        </div>
-
-        <div className="team-total-card">
-
-          💰 Jami: <b>0 so'm</b>
-
-        </div>
+        <TeamInfoPanel
+          team={team}
+          teamType={teamType}
+          workingCount={workingCount}
+        />
 
       </div>
 
---
+++ src/components/TeamsPanel/ActiveTeams/TeamInfoPanel.jsx
@@ -0,0 +1,67 @@
+import teamTypes, { teamTypeMap } from "../teamTypes";
+export default function TeamInfoPanel({
+    workingCount,
+    teamType,
+    team
+}) {
+
+  return (<>
+
+    {team.type === "washer" &&(
+
+    <div>
+
+      <div className="team-detail-summary">
+
+        <div className="team-detail-header"> 
+
+          <div>
+            <h2>{team.teamName}</h2>
+            <p>{teamType?.icon} {teamType?.title}</p>
+          </div>
+
+          <div className="team-online">
+            🟢 {workingCount} kishi faol
+          </div>
+
+        </div>
+
+        <div className="team-stats-grid">
+
+          <div className="team-stat-box">
+            <h4>🧼 Gilam</h4>
+            <p>0 kv • 0 so'm</p>
+          </div>
+
+          <div className="team-stat-box">
+            <h4>🛏 Adyol</h4>
+            <p>0 ta • 0 so'm</p>
+          </div>
+
+          <div className="team-stat-box">
+            <h4>🧵 Yakandoz</h4>
+            <p>0 ta • 0 so'm</p>
+          </div>
+
+          <div className="team-stat-box">
+            <h4>🪟 Parda</h4>
+            <p>0 m • 0 so'm</p>
+          </div>
+
+        </div>
+
+        <div className="team-total-card">
+
+          💰 Jami: <b>0 so'm</b>
+
+        </div>
+    
+      </div>
+
+    </div>
+
+    )}
+
+  </>)
+
+}
