53ca9a6b62feb6cc806ef3c2715df5071929c7dc
Nurmuhammad <nbaraimov@gmail.com>
2026-08-09 00:27:59 +0500

    bubble

--- src/Pages/Home/WelcomeCard/AnimatedBubbles.jsx
+++ src/Pages/Home/WelcomeCard/AnimatedBubbles.jsx
@@ -2,10 +2,10 @@ import "./AnimatedBubbles.css";
 
 export default function AnimatedBubbles() {
   const bubbles = [
-  { size: 45, top: "18%", left: "75%", delay: "0s" },
-  { size: 32, top: "11%", left: "65%", delay: "1.2s" },
-  { size: 18, top: "48%", left: "74%", delay: "2.4s" },
-  { size: 11, top: "52%", left: "88%", delay: "3.5s" },
+  { size: 45, top: "18%", left: "78%", delay: "0s" },
+  { size: 32, top: "11%", left: "68%", delay: "1.2s" },
+  { size: 20, top: "22%", left: "85%", delay: "2.4s" },
+  { size: 15, top: "20%", left: "70%", delay: "3.5s" },
   { size: 16, top: "55%", left: "80%", delay: "4.5s" },
   { size: 11, top: "50%", left: "70%", delay: "5.5s" }
 ];
