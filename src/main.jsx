85f043b536f4870efbb8921b32fc945e9407a9ac
Nurmuhammad <nbaraimov@gmail.com>
2026-08-26 23:27:04 +0500

    main info card

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
+import App from './Sakura/MainApp/App.jsx'
 
 import './index.css'
 import "./styles/variables.css" 
 import "./styles/layout.css";
-import "./styles/bottomNavigation.css";
+import "./Sakura/BottomNavigation/bottomNavigation.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
