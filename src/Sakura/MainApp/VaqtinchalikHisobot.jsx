e571a706e48d8d21cd15458b4dd2d09d63efa288
Nurmuhammad <nbaraimov@gmail.com>
2026-09-04 13:13:34 +0500

    vaqtinchalik hisobot

--- src/Sakura/MainApp/VaqtinchalikHisobot.jsx
+++ src/Sakura/MainApp/VaqtinchalikHisobot.jsx
@@ -16,6 +16,8 @@ import {
   Timestamp,
 } from "firebase/firestore";
 
+export default function VaqtinchalikHisobot (){
+
 const CARPET_COST_PER_M2 = 6000;
 const BLANKET_COST = 23500;
 const YAKANDOZ_COST = 22500;
@@ -204,3 +206,4 @@ export const buildTemporaryArchiveCards = (stats) => {
     },
   ];
 };
+}
