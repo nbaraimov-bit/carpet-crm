97c103475992a1f5c529294ee94a73f55fd6b4a5
Nurmuhammad <nbaraimov@gmail.com>
2026-09-15 00:28:42 +0500

    main info

--- src/Sakura/Pages/Home/MainInfoCard/MainInfoCard.jsx
+++ src/Sakura/Pages/Home/MainInfoCard/MainInfoCard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../../firebase";
+import { db } from "../../../../firebase";
 
 import "./MainInfoCard.css";
 
